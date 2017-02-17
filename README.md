389c3617a372faadd31dd12449bc0ee51967941e
Mattias Hagberg <pestbarn@gmail.com>
2017-02-17 12:38:36 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# MFC
-Music Folder Creator - create a basic hierarchy for your music folders
+MFC
+======
+Music Folder Creator - create a basic hierarchy for your music folders. UNIX and Windows systems supported.
+
+![](http://i.imgur.com/vCEPMnN.gif)
+
+### What it does
+Basically, these scripts are a way to start organizing your music folders, either in alphabetical order, or by genre/subgenre (currently only supported in Bash version).
+
+### How to use
+#### Bash
+You have two ways to use the Bash version - double clicking a file, or executing a shell script in your terminal.
+
+**In shell**
+ 1. Download [mfc.sh](mfc.sh)
+ 2. Move the file into a folder which will serve as your root music folder, e.g. `~/Music/My Library`
+ 3. Run the script with `. mfc.sh` and follow the prompts
+ 4. Done!
+
+**Outside shell**
+ 1. Download [mfc.command](mfc.command)
+ 2. Move the file into a folder which will serve as your root music folder, e.g. `~/Music/My Library`
+ 3. Double click the file, and follow the prompts
+ 4. Done!
+
+##### It doesn't work :(
+You may have to correct the file permissions. `chmod +x mfc.sh` will fix it.
+
+#### Windows Batch Script
+ 1. Download [MFC.exe](MFC.exe)
+ 2. Move the file into a folder which will serve as your root music folder, e.g. `C:\Music\My Library`
+ 3. Double click the file, and follow the prompts
+ 4. Done!
+
+##### Source: Windows Batch Script
+```DOS
+@ECHO OFF
+ECHO This program will create alphabetic folders...
+pause
+MD A-E
+MD A-E\A
+MD A-E\B
+MD A-E\C
+MD A-E\D
+MD F-J
+MD F-J\F
+MD F-J\G
+REM (and so on till the end)
+ECHO Done!
+pause
+```
+
+#### Contributing
+Contributions are more than welcome! The original `.bat` file for Windows was written way back in 2008, and I lost the original source code along the way. Also, I'm in no way a Bash expert, therefore there might be a lot of optimization opportunities here!
---
+++ mfc.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# This is a program that creates a music folder hierarchy
+
+echo
+echo "Hello, ""$USER"". This script will create a music folder hierarchy
+in the current folder:"
+pwd
+echo
+
+echo "Please choose an option below to create a hierarchy:"
+echo "(1) Alphabetical"
+echo "(2) By genre"
+echo
+
+echo -n "Make your choice and press [ENTER]: "
+read -r flevelsort
+echo
+
+if [ "$flevelsort" == "1" ]; then
+    echo "Creating alphabetical folder structure..."
+    $(mkdir -p {A-E,F-J,K-O,P-T,U-Z,"#0-9"})
+    $(mkdir -p A-E/{A,B,C,D,E})
+    $(mkdir -p F-J/{F,G,H,I,J})
+    $(mkdir -p K-O/{K,L,M,N,O})
+    $(mkdir -p P-T/{P,Q,R,S,T})
+    $(mkdir -p U-Z/{U,V,W,X,Y,Z})
+    $(mkdir -p "#0-9"/{0..9})
+    echo "Done! Thanks for using MFC :)"
+
+elif [ "$flevelsort" == "2" ]; then
+    echo "Which style would you like?"
+    echo "(1) Basic genres"
+    echo "(2) Basic genres + subgenres"
+    echo
+    echo -n "Make your choice and press [ENTER]: "
+    read -r genre
+    echo
+    echo "Creating genre folder structure..."
+    $(mkdir -p "Alternative Metal")
+    $(mkdir -p "Avant-garde Metal")
+    $(mkdir -p "Black Metal")
+    $(mkdir -p "Death Metal")
+    $(mkdir -p "Doom Metal")
+    $(mkdir -p "Extreme Metal")
+    $(mkdir -p "Folk Metal")
+    $(mkdir -p "Glam Metal")
+    $(mkdir -p "Gothic Metal")
+    $(mkdir -p "Grindcore")
+    $(mkdir -p "Industrial Metal")
+    $(mkdir -p "Metalcore")
+    $(mkdir -p "Neoclassical Metal")
+    $(mkdir -p "Neue Deutsche Härte")
+    $(mkdir -p "Power Metal")
+    $(mkdir -p "Progressive Metal")
+    $(mkdir -p "Speed Metal")
+    $(mkdir -p "Stoner Metal")
+    $(mkdir -p "Symphonic Metal")
+    $(mkdir -p "Thrash Metal")
+    $(mkdir -p "Traditional Heavy Metal")
+
+    if [ "$genre" == "2" ]; then
+        echo "Creating subgenre folder structure..."
+        $(mkdir -p "Alternative Metal/"{"Funk Metal","Nu Metal","Rap Metal"})
+        $(mkdir -p "Black Metal/"{"Ambient Black Metal","Atmospheric Black Metal","Blackgaze","Depressive Black Metal","Experimental Black Metal","Industrial Black Metal","Melodic Black Metal","NSBM","Progressive Black Metal","RABM","Raw Black Metal","Symphonic Black Metal","Viking Metal","War Metal"})
+        # No, I won't include Unblack/White Metal, because fuck you
+        $(mkdir -p "Death Metal/"{"Blackened Death Metal","Brutal Death Metal","Death 'n' Roll","Death Thrash","Melodic Death Metal","Old School Death Metal","Slam Death Metal","Symphonic Death Metal","Progressive Death Metal","Technical Death Metal"})
+        $(mkdir -p "Doom Metal/"{"Black-Doom","Death-Doom","Drone Metal","Epic Doom","Funeral Doom","Proto-Doom","Sludge Doom","Stoner Doom"})
+        $(mkdir -p "Folk Metal/"{"Celtic Metal","Medieval Metal","Pagan Metal","Pirate Metal"})
+        $(mkdir -p "Glam Metal/"{"Bubbleglam","Nu-glam","Power Pop","Sleaze"})
+        $(mkdir -p "Grindcore/"{"Deathgrind","Goregrind","Pornogrind"})
+        $(mkdir -p "Metalcore/"{"Deathcore","Mathcore","Thrashcore"})
+        $(mkdir -p "Progressive Metal/Djent")
+        $(mkdir -p "Thrash Metal/"{"Crossover Thrash","Groove Metal","Teutonic Thrash"})
+    fi
+    echo
+    echo "Done! Thanks for using MFC :)"
+fi
